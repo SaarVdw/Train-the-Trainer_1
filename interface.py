6a0035924bdb2b418f124db913afc1c45bb0fa15
vandewsa <saar.vandeweghe@stad.gent>
2021-08-17 13:54:59 +0200

    lettertypes toegevoegd

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 
 app = Tk()
 app.title("LEUKHEIDSMETER")
@@ -6,13 +7,15 @@ app.geometry="400x400"
 app.configure(bg='#f0c71e')
 
 def info():
-    info = Label(app, text='LEUKSTE TEAM', fg='#690f2d', bg='#f0c71e')
+    lettertype = font.Font(family='Braggadocio', name='appletterype', size=25, weight='normal', underline=1)
+    info = Label(app, text='Leukste Team', font=lettertype, fg='#690f2d', bg='#f0c71e')
     info.grid(row=2, column=5)
 
 DIT = Button(app, text="Data interventieteam", width=25, bg='#690f2d', fg='#ff6699', borderwidth=10, command=info)
 DIT.grid(row=2, column=2)
 
-Cool = Label(app, text="super leuk label", height=10, width=20, bg='#ff6699', fg="#0f0c63")
+highlightFont = font.Font(family='Chalkboard', name='appHighlightFont', size=25, weight='normal', underline = 1)
+Cool = Label(app, text="Super leuk label", font = highlightFont, height=10, width=20, bg='#ff6699', fg="#0f0c63")
 Cool.grid(row=4,column=3)
 
 input = Entry(app, width=30, borderwidth=10)
