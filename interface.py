a4c1f6f3c9772a554f330a19cf5cc8705e9643a5
vandewsa <saar.vandeweghe@stad.gent>
2021-08-17 11:29:51 +0200

    Interface

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ def info():
 DIT = Button(app, text="Data interventieteam", width=25, bg='#690f2d', fg='#ff6699', borderwidth=10, command=info)
 DIT.grid(row=2, column=2)
 
-Cool = Label(app, text="super leuk label", height=10, width=20, bg='#ff6699')
+Cool = Label(app, text="super leuk label", height=10, width=20, bg='#ff6699', fg="#0f0c63")
 Cool.grid(row=4,column=3)
 
 input = Entry(app, width=30, borderwidth=10)
@@ -23,7 +23,7 @@ namenlijst.set("kies de leukheid")
 Menutje = OptionMenu(app, namenlijst, 'Leuk', 'Leuker', 'Leukst')
 Menutje.grid(row=1,column=2)
 
-box = Checkbutton(app, text='hallo', bg='#ff6699', fg='#690f2d')
+box = Checkbutton(app, text='BERE NICE', bg='#ff6699', fg='#690f2d')
 box.grid(row=5,column=2)
 
 app.mainloop()
